2e3d73b744172e47d6c4c784994eedca3595ac8a
jcuush <jrcushing@sbcglobal.net>
2020-12-01 19:50:56 -0600

    made handlebars, model and controller following template from cat activity

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm = require("../config/orm");
+
+var burger = {
+    selectAll: function(cb) {
+      orm.selectAll(function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+          cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne(objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  module.exports = burger;
